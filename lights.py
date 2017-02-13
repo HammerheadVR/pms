6cdaa358e28c8197d21c4f05bdb7f74ce37def99
Bastiaan Grisèl <bastiaan.grisel@gmail.com>
2017-02-13 17:07:01 +0000

    Added radius and center estimation for mirror sphere

--- lights.py
+++ lights.py
@@ -1,5 +1,6 @@
 import pms
 import math
+from operator import sub
 
 import matplotlib
 # matplotlib.use('TkAgg')
@@ -9,9 +10,17 @@ from skimage import data
 from skimage.filters import threshold_otsu, threshold_adaptive
 from skimage.color import rgb2gray
 from skimage.measure import moments
+from skimage.feature import canny
+
+import numpy as np
+
+def centroid(im):
+    m = moments(im.astype(float), order=1)
+    return (m[1, 0] / m[0, 0], m[0, 1] / m[0, 0]) # (x,y)
 
 # Calculate light direction based on sphere
-def calculateLightDirection(image_location, radius, threshold = 0.95, show = True):
+# bounding_box: [x,y,width,height]
+def calculateLightDirection(image_location, center, radius, threshold = 0.95, show = False):
     # Threshold the image
     im = pms.imread(image_location)
     im_gray = rgb2gray(im)
@@ -19,27 +28,40 @@ def calculateLightDirection(image_location, radius, threshold = 0.95, show = Tru
     # global_thresh = threshold_otsu(im_gray)
     im_gray_thresh = im_gray > threshold
 
-    if show:
-        plt.imshow(im_gray_thresh, cmap='gray')
-        plt.show()
-
     # Cut out bounding box
     im_gray_thresh_box = im_gray_thresh
-    # im_gray_thresh_box = im_gray_thresh[x1:x2, y1:y2]
-
-    # Calculate center of pixels (hopefully only the highlight)
-    m = moments(im_gray_thresh_box.astype(float), order=1)
-    center = {'x': m[1, 0] / m[0, 0], 'y': m[0, 1] / m[0, 0]}
 
     if show:
-        plt.scatter(center['x'], center['y'])
+        plt.imshow(im_gray_thresh_box, cmap='gray')
+        plt.show()
+
+    # Calculate center of pixels (hopefully only the highlight)
+    specular_center = centroid(im_gray_thresh_box)
+
+    if show:
+        plt.scatter(specular_center[0], specular_center[1])
+        plt.scatter(center[0], center[1])
 
     # Calculate XYZ and return
-    x = (center['x'] - radius) / radius
-    y = (center['y'] - radius) / radius
-    z = math.sqrt(1.0 - pow(x, 2.0) - pow(y, 2.0))
+    x = (specular_center[0] - center[0]) / radius
+    y = (specular_center[1] - center[1]) / radius
+    z = math.sqrt(1.0 - pow(x, 2.0) - pow(y, 2.0)) # Equation of a sphere is x^2 + y^2 = 1
 
     return (x,y,z)
 
 if __name__ == '__main__':
-    print(calculateLightDirection("../psmImages/chrome/chrome.0.png", 200))
+    # Load mask image
+    im = pms.imread("../psmImages/chrome/chrome.mask.png")
+    im_gray = rgb2gray(im)
+
+    # Calculate centroid
+    center = centroid(im_gray)
+
+    # Calculate edge image
+    edges = canny(im_gray)
+    edge_pixels = np.transpose(np.nonzero(edges))
+
+    radii = map(lambda x: np.linalg.norm(map(sub, x, center)), edge_pixels)
+    radius = np.mean(radii)
+
+    print(calculateLightDirection("../psmImages/chrome/chrome.0.png", center, radius, 0.95, True))
