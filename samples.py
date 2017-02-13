c32a51bc215750ee882a57b29f1818bf709548d0
Bastiaan Grisèl <bastiaan.grisel@gmail.com>
2017-02-13 18:20:01 +0000

    Able to run samples

--- samples.py
+++ samples.py
@@ -3,10 +3,19 @@ from lights import *
 
 if __name__ == '__main__':
     # Process the mask image
-    (center, radius) = findCircleInMask("../psmImages/chrome/chrome.mask.png", True)
+    (center, radius) = findCircleInMask("../psmImages/chrome/chrome.mask.png", debug=False)
 
     # Calculate light directions of images
     base_dir = '../psmImages'
-    light_directions = [ calculateLightDirection(base_dir + '/chrome/chrome.%i.png' % i, center, radius) for i in range(12)]
+    light_directions = [ calculateLightDirection(base_dir + '/chrome/chrome.%i.png' % i, center, radius, debug=False) for i in range(12)]
 
     # Calculate Photometric stereo
+    name = 'rock'
+    images = [base_dir + '/%s/%s.%i.png' % (name,name,i) for i in range(12)]
+    normals = pms.photometricStereo(light_directions, images)
+
+    color = pms.colorizeNormals(normals)
+    # mask = pms.getImage(base_dir + '/%s/%s.mask.png' % (name,name))
+
+    plt.imshow(np.rot90(color,3))
+    plt.show()
